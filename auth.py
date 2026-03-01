1526a13192e8f590f25c5965c9eb6794408fd557
GANGISETTY LAKSHMI SNEHA <glakshmisneha2@gmail.com>
2026-03-01 14:56:17 +0530

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from database import get_connection
+
+def register_user(name, email, password, role):
+    conn = get_connection()
+    c = conn.cursor()
+    try:
+        c.execute("INSERT INTO users (name,email,password,role) VALUES (?,?,?,?)",
+                  (name,email,password,role))
+        conn.commit()
+        st.success("Registered Successfully")
+    except:
+        st.error("User already exists")
+    conn.close()
+
+def login_user(email, password):
+    conn = get_connection()
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE email=? AND password=?",(email,password))
+    user = c.fetchone()
+    conn.close()
+    return user
